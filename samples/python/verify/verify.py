97009c48db8eef664592e08fc93dd803d651ee7b
Doubango Telecom <github@doubango.org>
2025-03-06 02:00:04 +0100

    (v0.0.9) -> Improve speed & accuracy & add more documents
    - OCR engine got several modifications to improve speed and accuracy
       * use of float32 instead of float64
       * different activation parameters
       * SIMD intrinsics for the activation functions. Open sourced. ARM NEON at [1], AVX2 at [2] and SSE at [3][4]
       * better pattern matching enforcement using candidates from the LSTM proposals
       * better and faster binarization algorithm
       * fix some math overflow issues
    - Changed the default MAGSAC parameters. Check [5] and [6]. We highly recommend changing these values in your application.
    - Classifier is more accurate while using the same number of parameters
    - Support for more documents. Now we support more than 5027 documents. Complete list at [7]
    
    [1] https://github.com/DoubangoTelecom/compv/blob/master/base/math/intrin/arm/compv_math_activation_functions_intrin_neon.cxx
    [2] https://github.com/DoubangoTelecom/compv/blob/master/base/math/intrin/x86/compv_math_activation_functions_intrin_avx2.cxx
    [3] https://github.com/DoubangoTelecom/compv/blob/master/base/math/intrin/x86/compv_math_activation_functions_intrin_sse41.cxx
    [4] https://github.com/DoubangoTelecom/compv/blob/master/base/math/intrin/x86/compv_math_activation_functions_intrin_sse2.cxx
    [5] https://www.doubango.org/SDKs/kyc-documents-verif/docs/Configuration_options.html#magsac-sigma
    [6] https://www.doubango.org/SDKs/kyc-documents-verif/docs/Configuration_options.html#magsac-resampler
    [7] https://github.com/DoubangoTelecom/KYC-Documents-Verif-SDK/blob/main/Graphs.pdf

--- samples/python/verify/verify.py
+++ samples/python/verify/verify.py
@@ -48,9 +48,9 @@ JSON_CONFIG = {
     "ocr_tunning_apply_whitelist": True,
     "ocr_tunning_apply_blacklist": True,
     
-    "magsac_sigma": 2.0,
+    "magsac_sigma": 1.0,
     "magsac_max_iters": 5000,
-    "magsac_resampler": "prosac",
+    "magsac_resampler": "uniform",
     
     "tps_enabled": True,
     "tps_speed": 1,
